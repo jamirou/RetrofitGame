4af8769864f508413db5e53310dff8ec535271f6
Jamiro <jamirocristian15@gmail.com>
2023-09-25 17:04:27 -0300

    style for game image.

--- app/src/main/java/com/jamirodev/retrofitgameapp/components/BodyComponents.kt
+++ app/src/main/java/com/jamirodev/retrofitgameapp/components/BodyComponents.kt
@@ -1,7 +1,16 @@
 package com.jamirodev.retrofitgameapp.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.AlertDialogDefaults.shape
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -9,8 +18,15 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
+import com.jamirodev.retrofitgameapp.model.GameList
 import com.jamirodev.retrofitgameapp.util.Constants.Companion.COLOR_1
 import com.jamirodev.retrofitgameapp.util.Constants.Companion.COLOR_2
 import com.jamirodev.retrofitgameapp.util.Constants.Companion.COLOR_3
@@ -18,7 +34,7 @@ import com.jamirodev.retrofitgameapp.util.Constants.Companion.COLOR_3
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainTopBar(title: String, showBackButton: Boolean = false, onClickBackButton: () -> Unit) {
-    
+
     TopAppBar(
         title = { Text(text = title, color = Color.White, fontWeight = FontWeight.ExtraBold) },
         colors = TopAppBarDefaults.mediumTopAppBarColors(
@@ -27,13 +43,55 @@ fun MainTopBar(title: String, showBackButton: Boolean = false, onClickBackButton
         navigationIcon = {
             if (showBackButton) {
                 IconButton(onClick = { onClickBackButton() }) {
-                    Icon(imageVector =
-                    Icons.Default.ArrowBack,
+                    Icon(
+                        imageVector =
+                        Icons.Default.ArrowBack,
                         contentDescription = "Go back button",
-                        tint = Color(COLOR_3))
+                        tint = Color(COLOR_3)
+                    )
                 }
             }
         }
     )
-
 }
+
+@Composable
+fun CardGame(game: GameList, onClick: () -> Unit) {
+    Card(
+        shape = RoundedCornerShape(5.dp),
+        modifier = Modifier
+            .padding(10.dp)
+            .shadow(40.dp)
+            .clickable { onClick() }
+    ) {
+        Column {
+            MainImage(image = game.background_image)
+        }
+    }
+}
+
+@Composable
+fun MainImage(image: String) {
+//    val image = rememberImagePainter(data = image)
+    val image = rememberAsyncImagePainter(model = image)
+    Image(painter = image,
+        contentDescription = "Game Image",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(250.dp))
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
