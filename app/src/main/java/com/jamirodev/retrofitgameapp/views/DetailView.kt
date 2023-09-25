b40d0ec38b1690aef405f72bd3d850e7b244d8cc
Jamiro <jamirocristian15@gmail.com>
2023-09-25 20:03:34 -0300

    adding 2style

--- app/src/main/java/com/jamirodev/retrofitgameapp/views/DetailView.kt
+++ app/src/main/java/com/jamirodev/retrofitgameapp/views/DetailView.kt
@@ -1,10 +1,26 @@
 package com.jamirodev.retrofitgameapp.views
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.jamirodev.retrofitgameapp.components.MainImage
+import com.jamirodev.retrofitgameapp.components.MainTopBar
+import com.jamirodev.retrofitgameapp.util.Constants.Companion.COLOR_1
+import com.jamirodev.retrofitgameapp.util.Constants.Companion.COLOR_3
 import com.jamirodev.retrofitgameapp.viewModel.GamesViewModel
 
 @Composable
@@ -12,5 +28,39 @@ fun DetailView(viewModel: GamesViewModel, navController: NavController, id: Int)
     LaunchedEffect(Unit) {
         viewModel.getGameById(id)
     }
-    Text(text = viewModel.state.name, color = Color.Black)
+    Scaffold(
+        topBar = {
+            MainTopBar(title = viewModel.state.name, showBackButton = true) {
+                navController.popBackStack()
+            }
+        }
+    ) {
+        ContentDetailView(it, viewModel)
+    }
 }
+
+@Composable
+fun ContentDetailView(pad: PaddingValues, viewModel: GamesViewModel) {
+    val state = viewModel.state
+    Column(
+        modifier = Modifier
+            .padding(pad)
+            .background(Color(COLOR_1))
+    ) {
+        MainImage(image = state.background_image)
+        Spacer(modifier = Modifier.height(10.dp))
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 20.dp, end = 5.dp)
+        ) {
+
+        }
+    }
+}
+
+
+
+
+
