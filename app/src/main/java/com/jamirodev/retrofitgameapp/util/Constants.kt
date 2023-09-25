3ae6832b62004625924ad620b78b73385d70b135
Jamiro <jamirocristian15@gmail.com>
2023-09-25 15:08:54 -0300

    Import Rawg api

--- app/src/main/java/com/jamirodev/retrofitgameapp/util/Constants.kt
+++ app/src/main/java/com/jamirodev/retrofitgameapp/util/Constants.kt
@@ -1,8 +1,12 @@
 package com.jamirodev.retrofitgameapp.util
 
 class Constants {
-
     companion object {
-        const val BASE_URL = ""
+        const val BASE_URL = "https://api.rawg.io/api/"
+        const val ENDPOINT = "games"
+        const val API_KEY = "?key=376ac9acebc34c6298778cdb7531cfc4"
     }
 }
+
+// https://api.rawg.io/api/games?key=376ac9acebc34c6298778cdb7531cfc4
+// https://api.rawg.io/api/games/id?key=376ac9acebc34c6298778cdb7531cfc4
