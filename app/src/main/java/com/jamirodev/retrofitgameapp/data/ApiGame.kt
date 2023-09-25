b9c8ac86be14c279c43d075ac36fc2b6c527aec0
Jamiro <jamirocristian15@gmail.com>
2023-09-25 15:38:15 -0300

    interface configuration

--- app/src/main/java/com/jamirodev/retrofitgameapp/data/ApiGame.kt
+++ app/src/main/java/com/jamirodev/retrofitgameapp/data/ApiGame.kt
@@ -1,4 +1,13 @@
 package com.jamirodev.retrofitgameapp.data
 
+import com.jamirodev.retrofitgameapp.model.GamesModel
+import com.jamirodev.retrofitgameapp.util.Constants.Companion.API_KEY
+import com.jamirodev.retrofitgameapp.util.Constants.Companion.ENDPOINT
+import retrofit2.Response
+import retrofit2.http.GET
+
 interface ApiGame {
+
+    @GET(ENDPOINT + API_KEY)
+    suspend fun getGame(): Response<GamesModel>
 }
---
+++ app/src/main/java/com/jamirodev/retrofitgameapp/model/GamesModel.kt
@@ -0,0 +1,13 @@
+package com.jamirodev.retrofitgameapp.model
+
+data class GamesModel(
+    val count: Int,
+    val results: List<GameList>
+
+)
+
+data class GameList(
+    val id: Int,
+    val name: String,
+    val background_image: String
+)
