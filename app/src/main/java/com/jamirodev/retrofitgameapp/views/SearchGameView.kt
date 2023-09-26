97d401900a9fb44034ac1e818cc033bbec4b1c5f
Jamiro <jamirocristian15@gmail.com>
2023-09-25 21:34:49 -0300

    finish searchview and search bar

--- app/src/main/java/com/jamirodev/retrofitgameapp/views/SearchGameView.kt
+++ app/src/main/java/com/jamirodev/retrofitgameapp/views/SearchGameView.kt
@@ -1,12 +1,80 @@
 package com.jamirodev.retrofitgameapp.views
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.jamirodev.retrofitgameapp.viewModel.GamesViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchGameView(viewModel: GamesViewModel, navController: NavController) {
-    Text(text = "cualquercosa", color = Color.Black)
+
+    var query by remember {
+        mutableStateOf("")
+    }
+    var active by remember {
+        mutableStateOf(false)
+    }
+    val games by viewModel.games.collectAsState()
+
+    SearchBar(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        query = query,
+        onQueryChange = { query = it },
+        onSearch = { active = false },
+        active = active,
+        onActiveChange = { active = it },
+        placeholder = { Text(text = "Search") },
+        leadingIcon = {
+            Icon(imageVector = Icons.Default.Search, contentDescription = "Search icon")
+        },
+        trailingIcon = {
+            Icon(imageVector = Icons.Default.Close, contentDescription = "Close icon",
+                modifier = Modifier.clickable { navController.popBackStack() })
+        }
+    ) {
+        if (query.isNotEmpty()) {
+            val filterGames = games.filter { it.name.contains(query, ignoreCase = true) }
+            filterGames.forEach {
+                Text(text = it.name,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(bottom = 10.dp, start = 10.dp)
+                        .clickable {
+                            navController.navigate("DetailView/${it.id}")
+                        }
+
+                )
+            }
+        }
+    }
+
 }
+
+
+
+
+
+
+
