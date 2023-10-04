2acaf7ce732a180404ed4acd226871fab0380ea2
Jamiro <jamirocristian15@gmail.com>
2023-10-04 18:33:23 -0300

    fixing bugs

--- app/src/main/java/com/jamirodev/retrofitgameapp/util/Constants.kt
+++ app/src/main/java/com/jamirodev/retrofitgameapp/util/Constants.kt
@@ -4,14 +4,13 @@ class Constants {
     companion object {
         const val BASE_URL = "https://api.rawg.io/api/"
         const val ENDPOINT = "games"
-        const val API_KEY = "yourAPIKEY"
+        const val API_KEY = "?key=376ac9acebc34c6298778cdb7531cfc4"
 
 
         const val COLOR_1 = 0xFFEE9322.toInt() //orange
         const val COLOR_2 = 0xFFD83F31.toInt() //red
         const val COLOR_3 = 0xFF219C90.toInt() //blue/green
         const val COLOR_4 = 0xFFE3F6FF.toInt() //white retro
-        const val COLOR_5 = 0xFF40128B.toInt() //purple
 
     }
 }
