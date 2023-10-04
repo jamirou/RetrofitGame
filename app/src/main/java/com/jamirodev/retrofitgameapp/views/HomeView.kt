4ac703814f7bd2c66885f203d7777690357d9599
Jamiro <jamirocristian15@gmail.com>
2023-10-04 18:55:23 -0300

    added search engine

--- app/src/main/java/com/jamirodev/retrofitgameapp/views/HomeView.kt
+++ app/src/main/java/com/jamirodev/retrofitgameapp/views/HomeView.kt
@@ -1,18 +1,29 @@
 package com.jamirodev.retrofitgameapp.views
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.jamirodev.retrofitgameapp.components.CardGame
@@ -25,7 +36,7 @@ import com.jamirodev.retrofitgameapp.viewModel.GamesViewModel
 fun HomeView(viewModel: GamesViewModel, navController: NavController) {
     Scaffold(
         topBar = {
-            MainTopBar(title = "GAMES", onClickBackButton = {  }) {
+            MainTopBar(title = "GAMES", onClickBackButton = { }) {
                 navController.navigate("SearchGameView")
             }
         }
@@ -37,20 +48,47 @@ fun HomeView(viewModel: GamesViewModel, navController: NavController) {
 @Composable
 fun ContentHomeView(viewModel: GamesViewModel, pad: PaddingValues, navController: NavController) {
     val games by viewModel.games.collectAsState()
-    LazyColumn(
+    var search by remember { mutableStateOf("") }
+
+    Column(
         modifier = Modifier
-            .padding(pad)
-            .background(Color(COLOR_1))
+            .padding(pad),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        items(games) { item ->
-            CardGame(item) {
-                navController.navigate("DetailView/${item.id}")
+
+        TextField(
+            value = search,
+            onValueChange = { search = it },
+            label = { Text(text = "Search") },
+            keyboardOptions = KeyboardOptions.Default.copy(
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = {
+
+                }
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 10.dp, end = 10.dp)
+        )
+
+        LazyColumn(
+            modifier = Modifier
+                .background(Color(COLOR_1))
+        ) {
+            items(games) { item ->
+                CardGame(item) {
+                    navController.navigate("DetailView/${item.id}")
+                }
+                Text(
+                    text = item.name,
+                    fontWeight = FontWeight.ExtraBold,
+                    color = Color(COLOR_4),
+                    modifier = Modifier.padding(start = 10.dp)
+                )
             }
-            Text(text = item.name,
-                fontWeight = FontWeight.ExtraBold,
-                color = Color(COLOR_4),
-                modifier = Modifier.padding(start = 10.dp)
-            )
         }
     }
 }
