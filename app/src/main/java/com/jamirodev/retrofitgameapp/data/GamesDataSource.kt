d673de75478bbe1afa01bf35d7299594ceb2d9df
Jamiro <jamirocristian15@gmail.com>
2023-10-10 09:58:40 -0300

    adding paging

--- app/src/main/java/com/jamirodev/retrofitgameapp/data/GamesDataSource.kt
+++ app/src/main/java/com/jamirodev/retrofitgameapp/data/GamesDataSource.kt
@@ -1,4 +1,30 @@
 package com.jamirodev.retrofitgameapp.data
 
-class GamesDataSource {
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.jamirodev.retrofitgameapp.model.GameList
+import com.jamirodev.retrofitgameapp.repository.GamesRepository
+
+class GamesDataSource(private val repo: GamesRepository) : PagingSource<Int, GameList>() {
+    override fun getRefreshKey(state: PagingState<Int, GameList>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            val anchorPage = state.closestPageToPosition(anchorPosition)
+            anchorPage?.prevKey?.plus(1) ?: anchorPage?.nextKey?.minus(1)
+        }
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, GameList> {
+        return try {
+            val nextPageNumber = params.key ?: 1
+            val response = repo.getGamesPaging(nextPageNumber, 3)
+            LoadResult.Page(
+                data = response.results,
+                prevKey = null,
+                nextKey = if (response.results.isNotEmpty()) nextPageNumber + 1 else null
+            )
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        }
+    }
+
 }
